8e069803223939aacd3270af5b95b7e368e305f1
raygdev <raymond.greeley.dev@gmail.com>
2025-09-11 21:34:28 -0400

    Allow api provider to implement Source class

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,12 +1,14 @@
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
+import 'repository.dart';
 
 final _root = 'https://hacker-news.firebaseio.com/v0';
 
-class NewsApiProvider {
+class NewsApiProvider implements Source {
   Client client = Client();
 
+  @override
   Future<List<int>> fetchTopIds() async {
     final response = await client.get(Uri.parse('$_root/topstories.json'));
     final ids = json.decode(response.body);
@@ -14,6 +16,7 @@ class NewsApiProvider {
     return ids.cast<int>();
   }
 
+  @override
   Future<ItemModel> fetchItem(int id) async {
     final response = await client.get(Uri.parse('$_root/item/$id.json'));
     final parsedJson = json.decode(response.body);
