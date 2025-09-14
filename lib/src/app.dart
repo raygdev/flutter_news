59c7ddb55844d80e2eddaccb0bda2f992421e016
raygdev <raymond.greeley.dev@gmail.com>
2025-09-13 20:28:53 -0400

    Use NewsList widget in app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
+import 'screens/news_list.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hacker News API',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hacker News'),
-        ),
-        body: Container(
-          child: Text('Contianer rendering'),
-        ),
-      ),
+      home: NewsList(),
     );
   }
 }
